5f26428058eaee0f48886006ef241f1d0e9c9085
takacs <davidetkcs@gmail.com>
2022-07-11 01:20:11 +0200

    Add goat image

---
+++ src/components/Goat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import goatImage from '../public/goat.png';
+
+export default function Navbar () { 
+    return (
+        <div className="goat">
+            <img src={ goatImage } />
+        </div>
+    )
+}
