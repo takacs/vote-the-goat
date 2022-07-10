6a7c8e61756b1f874096874a155dc7ead0bdb531
takacs <davidetkcs@gmail.com>
2022-07-11 01:18:17 +0200

    Add goat mascot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
 import Game from './components/Game.js'
+import Goat from './components/Goat.js'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Goat />
       <Game />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navbar () { 
     return (
         <header className="navbar">
-            <h2 className="navbar--title">Who's the Goat?</h2>
+            <h2 className="navbar--title">Vote the GOAT</h2>
         </header>
     )
 }
